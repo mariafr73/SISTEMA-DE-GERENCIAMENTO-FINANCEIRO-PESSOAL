00405d788b43754f3060ceab7cd95ff31a4d848d
Levi2804 <leviaraujomaianeto@gmail.com>
2026-06-04 20:13:49 -0300

    Adicionando TelaDespesa no Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,7 @@ import src.model.Sessao;
 import src.views.TelaCategoria;
 import src.views.TelaRenda;
 import src.views.TelaUsuario;
+import src.views.TelaDespesa;
 
 public class Main {
     public static void main(String[] args) {
